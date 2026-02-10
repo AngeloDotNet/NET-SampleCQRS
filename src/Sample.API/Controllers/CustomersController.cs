af98171af89cb1251230cd5e51bcacdb7a236080
Angelo Pirola <angelo9871@gmail.com>
2026-02-11 00:18:17 +0100

    Add CustomersController with create and get endpoints
    Introduced CustomersController using MediatR for command handling and ReadDbContext for data access. Added POST endpoint to create customers and GET endpoint to retrieve customers by ID. Included necessary using directives.

---
+++ src/Sample.API/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sample.API.Data;
+using Sample.API.Models.Command;
+
+namespace Sample.API.Controllers;
+
+[ApiController]
+[Route("api/customers")]
+public class CustomersController(IMediator mediator, ReadDbContext readDb) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync([FromBody] CreateCustomerCommand cmd)
+    {
+        var id = await mediator.Send(cmd);
+        return CreatedAtAction(nameof(GetAsync), new { id }, new { id });
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetAsync(Guid id)
+    {
+        var c = await readDb.Customers.FindAsync(id);
+
+        if (c == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(c);
+    }
+}
