ea08729bd30223c9bdc5971caadbb321c62e3e3e
Angelo Pirola <angelo9871@gmail.com>
2026-02-11 00:19:57 +0100

    Add CustomerCreatedEvent class for event metadata
    Introduced CustomerCreatedEvent in the Sample.API.Events namespace.
    This class includes EventId, AggregateId, OccurredAt, and a Data
    property for storing the event payload as JSON. It will be used to
    represent customer creation events with relevant metadata.

---
+++ src/Sample.API/Events/CustomerCreatedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Sample.API.Events;
+
+public class CustomerCreatedEvent
+{
+    public Guid EventId { get; set; }
+    public Guid AggregateId { get; set; }
+    public DateTime OccurredAt { get; set; }
+    public string Data { get; set; } = default!; // json payload
+}
