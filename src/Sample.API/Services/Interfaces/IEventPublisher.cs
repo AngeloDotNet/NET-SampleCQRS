38dc07efdfcd7cc2d07af8d8ad04d62219820cc2
Angelo Pirola <angelo9871@gmail.com>
2026-02-12 00:02:27 +0100

    Add IEventPublisher and IRabbitMqConnection interfaces
    Introduce two new interfaces in Sample.API.Services.Interfaces:
    - IEventPublisher for publishing raw event messages asynchronously.
    - IRabbitMqConnection for managing RabbitMQ connections.

---
+++ src/Sample.API/Services/Interfaces/IEventPublisher.cs
@@ -0,0 +1,6 @@
+﻿namespace Sample.API.Services.Interfaces;
+
+public interface IEventPublisher
+{
+    Task PublishRawAsync(string type, Guid messageId, ReadOnlyMemory<byte> body, CancellationToken cancellationToken = default);
+}
---
+++ src/Sample.API/Services/Interfaces/IRabbitMqConnection.cs
@@ -0,0 +1,8 @@
+﻿using RabbitMQ.Client;
+
+namespace Sample.API.Services.Interfaces;
+
+public interface IRabbitMqConnection : IDisposable
+{
+    IConnection GetConnection();
+}
