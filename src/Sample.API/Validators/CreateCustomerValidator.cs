42a805743a26f7e58db73922551cfc39fc2e7f33
Angelo Pirola <angelo9871@gmail.com>
2026-02-12 00:03:21 +0100

    Add CreateCustomerValidator for customer input validation
    Introduced CreateCustomerValidator using FluentValidation to ensure that customer creation requests have a non-empty Name and a valid, non-empty Email. Custom error messages are provided for each validation rule.

---
+++ src/Sample.API/Validators/CreateCustomerValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Sample.API.Models.Command;
+
+namespace Sample.API.Validators;
+
+public class CreateCustomerValidator : AbstractValidator<CreateCustomerCommand>
+{
+    public CreateCustomerValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Valid Email is required");
+    }
+}
